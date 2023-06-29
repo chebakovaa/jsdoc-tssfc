af1f4372cbec0c147b2d20c09d93b2f847d27896
chbkv <achbkv@gmail.com>
2023-06-29 16:14:32 +0500

    Initial commit

---
+++ typescript.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const ts = require('typescript')
+
+const typeConverter = require('./typescript/type-converter')
+
+exports.handlers = {
+  newDoclet: function({ doclet }) {
+    if (doclet.tags && doclet.tags.length > 0) {
+      const categoryTag = doclet.tags.find(tag => tag.title === 'optional')
+      if (categoryTag) {
+        doclet.optional = true
+      }
+    }
+  },
+
+  beforeParse: function(e) {
+    if (['.ts', '.tsx', '.vue'].includes(path.extname(e.filename))) {
+      if (['.vue'].includes(path.extname(e.filename))) {
+        var script = e.source.match(new RegExp('<script lang="ts">(.*?)</script>', 's'))
+        e.source = script[1]
+      }
+
+      // adding const a = 1 ensures that the comments always will be copied,
+      // even when there is no javascript inside (just interfaces)
+      let result = ts.transpileModule('const _____a = 1; \n' + e.source, {
+        compilerOptions: {
+          target: 'esnext',
+          esModuleInterop: true,
+          jsx: path.extname(e.filename) === '.tsx' ? 'react' : null,
+        }
+      })
+      
+      const types = typeConverter(e.source, e.filename)
+      let src = result.outputText
+      e.source = src + '\n' + types
+    }
+  }
+}
